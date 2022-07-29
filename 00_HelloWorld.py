32af639ce497f86e985481e8752f31547f968542
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-29 18:22:14 +0530

    Hello World - Python IDLE intro

--- 00_HelloWorld.py
+++ 00_HelloWorld.py
@@ -1,1 +1,13 @@
 print ('Hello World')
+
+# >>> : Triple Chevron or Prompt or Command Prompt
+
+# IDE package include : 
+# Color highlight 
+# Editor
+# Debugger
+# Shell 
+
+# Tab is used to auto complete commands in shell 
+# ALt N -> Next 
+# ALt P -> Previous command
