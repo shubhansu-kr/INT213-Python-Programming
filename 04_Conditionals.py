914394f0954a78e9632cdf29e843788fc269d394
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-09 10:58:09 +0530

    Conditionals

---
+++ 04_Conditionals.py
@@ -0,0 +1,27 @@
+# Conditonal Statements
+
+# Syntax 
+# if (condition): 
+#     (Block statement)
+
+if 1:
+    print('It\'s True')
+else:
+    print('It\'s False')
+
+
+if 0:
+    print('It\'s True')
+else:
+    print('It\'s False')
+
+# Blocks in python is created/differentiated using indentation
+# indentation: Leaving Tab space
+
+
+age = int(input('Enter your age '))
+
+if (age >= 18): 
+    print('You can vote in the election')
+else: 
+    print('You cannot vote in the election')
