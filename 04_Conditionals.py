5c2b7ea0de8b8ebbfdec9d01f15d614d8d6984f9
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-09 11:23:00 +0530

    Conditional

--- 04_Conditionals.py
+++ 04_Conditionals.py
@@ -25,3 +25,39 @@ if (age >= 18):
     print('You can vote in the election')
 else: 
     print('You cannot vote in the election')
+
+
+# Conditional on list 
+
+courses = ['Python', 'DBMS', 'DSA']
+print(courses)
+
+course_name = input('Enter Your course name ') 
+
+# string cannot be compared to list, so falsy value 
+if (course_name == courses): 
+    print(course_name)
+
+# in is used to loop in courses : IF course name is in list courses print name
+if (course_name in courses): 
+    print(course_name)
+
+name = 'Python' 
+user_name = input('Enter Name ')
+
+if (user_name == name): 
+    print ('Saanp paal rha tha')
+else: 
+    print ('Hello')
+
+# Nested if & laddered if 
+course_code = input('Enter your course code(Uppercase) ')
+
+if (course_code == 'INT213'):
+    print('Python')
+elif (course_code == 'INT306'): 
+    print('Database')
+elif (course_code == 'CSE205'): 
+    print('DSA')
+else: 
+    print('You are not learning')
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+course_code = input('Enter your course code(Uppercase) ')
+
+if (course_code == 'INT213'):
+    print('Python')
+elif (course_code == 'INT306'): 
+    print('Database')
+elif (course_code == 'CSE205'): 
+    print('DSA')
+else: 
+    print('You are not learning')
