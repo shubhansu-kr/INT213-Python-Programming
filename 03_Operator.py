9dfd2c459250e8e46313d3d6baf8b74379ca7b74
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-09 10:42:09 +0530

    Operators

--- 03_Operator.py
+++ 03_Operator.py
@@ -1,4 +1,13 @@
 # Logical operator 
 # 1. And
 # 2. Or
+# 3. not
+
+sem1 = 91
+sem2 = 85 
+
+eligile1 = sem1 > 90 and sem2 > 90
+eligible2 = sem1 > 90 or sem2 > 90 
+
+print (eligible2, eligile1)
 
