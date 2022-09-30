8a93753ceaca81d32250f99ac5a0181164128fee
Shubhansu kumar <shubhansu2021@gmail.com>
2022-09-30 11:33:47 +0530

    Multi level inheritance
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 01_Oops/05_Practice.py
+++ 01_Oops/05_Practice.py
@@ -6,3 +6,34 @@
 # Print the details of an employee with its name, age, address, company, 
 # designation and salary 
 
+class Person: 
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age 
+
+class Company(Person): 
+    def __init__(self, name, age, worksWith, location) -> None:
+        super().__init__(name, age)
+        self.worksWith = worksWith
+        self.location = location
+
+class Employee(Company): 
+    def __init__(self, name, age, worksWith, location, des, sal) -> None:
+        super().__init__(name, age, worksWith, location)
+        self.salary = sal
+        self.designation = des
+
+    def details(self): 
+        print(
+            f"""
+            Name        :   {self.name}
+            Age         :   {self.age}
+            WorksWith   :   {self.worksWith}
+            Location    :   {self.location}
+            Designation :   {self.designation}
+            Salary      :   {self.salary}
+            """
+        )
+
+e = Employee('shubhansu-kr', 19, 'Micro', 'Hyderabad', 'L4', 4243454)
+e.details()
--- 01_Oops/06_Inheritance.py
+++ 01_Oops/06_Inheritance.py
@@ -1,3 +1,39 @@
+
+class A: 
+    a = ''
+    def __init__(self, a) -> None:
+        self.a = a
+        print('Constructor A')
+    def displayA(): 
+        print("Instance Method A")
+
+class C: 
+    t = ''
+    def __init__(self, t) -> None:
+        self.t = t
+        print('Constructor C')
+    def displayC(): 
+        print('Instance C')
+
+class B(A, C): 
+    s = ''
+    def __init__(self, a, s) -> None:
+        super().__init__(a)
+        self.s = s
+        print("Constructor B")
+    def displayB():
+        print("Instance Method B")
+
+# If the derived class does not have a constructor, the base class 
+# constructor which is inherited by the derived class is called.
+b = B('consA', 'consB')
+
+# IF both class has their own constructor, only derived class constructor 
+# is called
+# To call the constructor of base class super is used 
+
+# Now to initialise the members of the class 
+
 class First: 
     def __init__(self) -> None:
         super(First, self).__init__()
@@ -32,3 +68,7 @@ b = THird()
 # Super init will call the init function of the first class and if the first
 # arg class does not have any init function, it will look for another class 
 
+# Method Resolution Order 
+# Depth first search 
+
+# It will search the constructor in the current class
