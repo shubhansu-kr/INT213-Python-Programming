cc10f0903b9a88202952dda51277f7163c96ab5b
Shubhansu kumar <shubhansu2021@gmail.com>
2022-10-21 10:14:35 +0530

    Practice.cpp
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 02_Utilities/04_Practice.py
@@ -0,0 +1,7 @@
+# Write a python code to read a file word by word and store the 
+# result in a list 
+
+question = "Write a python code to read a file word by word and store the result in a list"
+
+f = open('dummy.txt', 'w')
+f.write(question)
