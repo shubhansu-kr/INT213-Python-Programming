59cb6a453f48678b99c98397fd2c43f2d58b207f
Shubhansu kumar <shubhansu2021@gmail.com>
2022-11-25 19:53:36 +0530

    Operator
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ Practice/01_Operator.py
@@ -0,0 +1,57 @@
+# Assignment. 
+# 
+
+# Arithemetic
+# + 
+# -
+# /
+# %
+# *
+# //
+# **
+# 
+# Logical 
+# and
+# or
+# not
+# 
+# Assignment
+# =
+# +=
+# -=
+# /=
+# *=
+# //=
+# **=
+# |=
+# &=
+# ^=
+# ~=
+# >>=
+# <<=
+# 
+# Relational Operator
+# ==
+# >
+# <
+# >=
+# <=
+# !=
+# 
+# Identity
+# is
+# is not
+# 
+# Bitwise 
+# ~
+# |
+# &
+# ^
+# >>
+# <<
+# 
+# Member
+# in 
+# not in
+# 
+ 
